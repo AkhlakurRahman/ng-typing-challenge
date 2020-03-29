0b9ee61b2a63ded04528e9282f3c0d8329835ad6
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-03-29 15:19:35 +0600

    typing check done while typing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomParagraph = lorem.paragraph();
+  enteredText = "";
+
+  onInputChange(value: string) {
+    this.enteredText = value;
+  }
+
+  classComparison(letter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return "pending";
+    }
+
+    return letter === enteredLetter ? "correct" : "incorrect";
+  }
 }
